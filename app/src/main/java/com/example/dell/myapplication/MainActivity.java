074db04e661c92e4cfc0f8f5ff50abb20e07fd7d
nikhil <ntnikhilesh@gmail.com>
2016-10-20 20:43:14 +0530

    First commit

---
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.dell.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView showdistance=(TextView)findViewById(R.id.showdistace);
+
+      double dist=  calculateDistance(12.92021997582485,77.61453927203593,12.916015,77.58572800000002,"K");
+        String val=String.valueOf(dist);
+        showdistance.setText(val);
+    }
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2, String unit)
+    {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") {
+            dist = dist * 1.609344;
+        } else if (unit == "M") {
+            dist = dist * 0.8684;
+        }
+        return (dist);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts decimal degrees to radians             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double deg2rad(double deg)
+    {
+        return (deg * Math.PI / 180.0);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts radians to decimal degrees             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private double rad2deg(double rad)
+    {
+        return (rad * 180.0 / Math.PI);
+    }
+}
